1c90d6d99f5d3ae780dee276862f7089deca7724
JC <bonassin@gmail.com>
2021-02-12 17:07:22 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
-(1,"Mr. Chocolate", 15, "F", "Dark Blue", "calm", 0),
-(2,"Rowdy", 4, "F", "Brown", "intense", 1),
-(3,"Tabitha", 10, "M", "black", "Nice", 1),
-(4,"Sargeant Brown", 23, "M", "Purple", "Slimy", 0),
-(5,"Melissa", 3, "F", "dark brown", "goofy", 1),
-(6,"Grinch", 25, "M", "green", "Grinchy", 1),
-(7,"Wendy", 8, "F", "Black", "naive", 1),
-(8,null, 19, "F", "yellow", "aggressive", 0);
+(1,"Mr. Chocolate", 15, "F", "Dark Blue", "relax", 1),
+(2,"Rowdy", 4, "F", "Brown", "cheerfull", 1),
+(3,"Tabitha", 10, "M", "black", "calm", 0),
+(4,"Sargeant Brown", 23, "M", "Purple", "crazy", 1),
+(5,"Melissa", 3, "F", "dark brown", "mad", 0),
+(6,"Grinch", 25, "M", "green", "friendly", 1),
+(7,"Wendy", 8, "F", "Black", "crazy", 1),
+(8,null, 19, "F", "yellow", "goofy", 0);
